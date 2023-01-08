d7c6e45cf1d3fb13e8f98670d2cb35ea923152c5
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-01-08 21:27:23 +0600

    2. More Object Types

--- Types/objects.ts
+++ Types/objects.ts
@@ -4,14 +4,29 @@
 //   age: 0.5,
 // };
 
-function printName(person: { first: string; last: string }): void {
-  console.log(`${person.first} ${person.last}`);
+// function printName(person: { first: string; last: string }): void {
+//   console.log(`${person.first} ${person.last}`);
+// }
+
+// printName({ first: "sanjit", last: "sarkar" });
+
+// let coordinate: { x: number; y: number } = { x: 2, y: 5 };
+
+// function randomCoordinate(): { x: number; y: number } {
+//   return { x: Math.random(), y: Math.random() };
+// }
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+let coordinate: Point = { x: 34, y: 3 };
+
+function randomCoordinate(): Point {
+  return { x: Math.random(), y: Math.random() };
 }
 
-printName({ first: "sanjit", last: "sarkar" });
-
-let coordinate: { x: number; y: number } = { x: 2, y: 5 };
-
-function randomCoordinate(): { x: number; y: number } {
-  return { x: Math.random(), y: Math.random() };
+function doublePoint(point: Point): Point {
+  return { x: point.x * 3, y: point.y * 2 };
 }
